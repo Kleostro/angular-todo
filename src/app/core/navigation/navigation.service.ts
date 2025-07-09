4eec99ed75f65f699c4d4467d4b24e829a9be40e
Max <133934232+Kleostro@users.noreply.github.com>
2025-07-09 15:18:39 +0300

    feat: display a task completion message

--- src/app/core/navigation/navigation.service.ts
+++ src/app/core/navigation/navigation.service.ts
@@ -1,6 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Location } from '@angular/common';
+import { inject, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { APP_ROUTE } from '@/app/core/navigation/routes';
 
 @Injectable({
   providedIn: 'root',
 })
-export class NavigationService {}
+export class NavigationService {
+  private readonly location = inject(Location);
+  private readonly router = inject(Router);
+
+  public goBack(): void {
+    this.location.back();
+  }
+
+  public navigateToTaskById(taskId: number): void {
+    this.router.navigate([APP_ROUTE.TASKS, taskId]);
+  }
+}
---
+++ src/app/shared/constants/message.ts
@@ -0,0 +1,4 @@
+export const MESSAGE = {
+  DELETE_TASK_SUCCESS: 'Задача успешно удалена',
+  TASK_NOT_FOUND: 'Задача не найдена',
+};
