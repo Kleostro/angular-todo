aa74b0e67ab19a15baa052610b0fc3ffbe06a570
Max <133934232+Kleostro@users.noreply.github.com>
2025-07-09 17:14:09 +0300

    refactor: remove the magical task status strings

--- src/app/tasks/models/task.model.ts
+++ src/app/tasks/models/task.model.ts
@@ -1,6 +1,13 @@
+export const TASK_STATUS = {
+  COMPLETED: 'completed',
+  PENDING: 'pending',
+} as const;
+
 export interface Task {
   description?: string;
   id: number;
-  status: 'completed' | 'pending';
+  status: TaskStatus;
   title: string;
 }
+
+export type TaskStatus = (typeof TASK_STATUS)[keyof typeof TASK_STATUS];
